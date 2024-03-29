0d783c7c5e753e9b99cd493252c8b7e008ee9989
XanderMcGowan <xander.mcgowan@gmail.com>
2024-03-29 12:20:31 -0500

    framed top area

---
+++ art-gallery/src/components/Home.js
@@ -0,0 +1,27 @@
+
+
+const Home = () => {
+
+
+    return(
+        <div className='main-container'>
+                <a href="">
+                    <div className='button-location'>
+
+                    </div>
+                        
+                </a>
+            <div className='sub-container-top'>
+                <div className="sct-black"></div>
+                <div className="sct-photo"></div>
+                <div className="sct-white"></div>
+
+            </div>
+            <div className='sub-container-bottom'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
