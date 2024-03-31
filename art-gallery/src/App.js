895cea8af837f9038a978bd96eb8431995e7dcec
XanderMcGowan <xander.mcgowan@gmail.com>
2024-03-31 14:05:33 -0500

    added media query, famed top section of mobile

--- art-gallery/src/App.js
+++ art-gallery/src/App.js
@@ -1,10 +1,14 @@
 import Home from './components/Home'
+import Mobile from './components/Mobile';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <useMediaQuery maxWidth={600}>
+ <Mobile />
+</useMediaQuery>
     </div>
   );
 }
---
+++ art-gallery/src/components/Mobile.js
@@ -0,0 +1,25 @@
+import Button from './Button'
+
+const Mobile = () => {
+    return (
+        <div className="mobile-container">
+            <div className="container-top">
+                <div className="container-top-photo">
+                </div>
+                <h1 className="container-top-title">modern 
+                <br></br>
+                art gallery</h1>
+                <p className="container-top-copy">
+                The arts in the collection of the Modern Art Gallery all started 
+  from a spark of inspiration. Will these pieces inspire you? Visit 
+  us and find out.
+                </p>
+                <Button />
+            </div>
+            <div className="container-bottom"></div>
+            <div className="footer"></div>
+        </div>
+    )
+}
+
+export default Mobile
