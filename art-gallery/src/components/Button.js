3b13cc5b08bfd666928126f9ace564fc05051151
XanderMcGowan <xander.mcgowan@gmail.com>
2024-03-30 22:58:10 -0500

    added top section: text, title and button

---
+++ art-gallery/src/components/Button.js
@@ -0,0 +1,14 @@
+
+
+const Button = () => {
+    return(
+        <div className="button">
+        <div className="button-el-1">Our Location</div>
+        <div className="button-el-2">
+            <div className="button-icon"></div>
+        </div>
+      </div>
+    )
+}
+
+export default Button
--- art-gallery/src/components/Home.js
+++ art-gallery/src/components/Home.js
@@ -1,38 +1,42 @@
-
+import Button from "./Button";
 
 const Home = () => {
-
-
-    return(
-        <div className='main-container'>
-                <a href="">
-                    <div className='button-location'>
-
-                    </div>
-                        
-                </a>
-            <div className='sub-container-top'>
-                <div className="sct-black"></div>
-                <div className="sct-photo"></div>
-                <div className="sct-white"></div>
-
-            </div>
-            <div className='sub-container-bottom'>
-                <div className="flex-top">
-                    <div className="ft-text"></div>
-                    <div className="ft-photo"></div>
-                </div>
-                <div className="flex-bottom">
-                    <div className="fb-sub-1"></div>
-                    <div className="fb-sub-2">
-                        <div className="fb-sub-2-photo1"></div>
-                        <div className="fb-sub-2-photo2"></div>
-                    </div>
-                </div>
-            </div>
-            <footer></footer>
+  return (
+    <div className="main-container">
+      <div className="sub-container-top">
+        <div className="sct-black">
+          <h1 className="text-title">
+            Modern
+            <br></br>
+            Art Gallery
+          </h1>
         </div>
-    )
-}
+        <div className="sct-photo"></div>
+        <div className="sct-white">
+          <p className="text-sct-upper">
+            The arts in the collection of the Modern Art Gallery all started
+            from a spark of inspiration. Will these pieces inspire you? Visit us
+            and find out.
+          </p>
+        <Button />
+                </div>
+      </div>
+      <div className="sub-container-bottom">
+        <div className="flex-top">
+          <div className="ft-text"></div>
+          <div className="ft-photo"></div>
+        </div>
+        <div className="flex-bottom">
+          <div className="fb-sub-1"></div>
+          <div className="fb-sub-2">
+            <div className="fb-sub-2-photo1"></div>
+            <div className="fb-sub-2-photo2"></div>
+          </div>
+        </div>
+      </div>
+      <footer></footer>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
